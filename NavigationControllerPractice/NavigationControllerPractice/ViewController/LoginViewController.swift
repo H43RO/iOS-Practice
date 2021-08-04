b19f2715dad75eaab4303ddbab7bfe4fcafd0841
H43RO <h2is1234@naver.com>
2021-08-04 19:12:49 +0900

    Navigation Controller 조작

--- NavigationControllerPractice/NavigationControllerPractice/ViewController/LoginViewController.swift
+++ NavigationControllerPractice/NavigationControllerPractice/ViewController/LoginViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // 뷰가 생성되었을 때
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 상단 네비게이션 바 부분을 숨김 처리
+        self.navigationController?.isNavigationBarHidden = true
     }
 }
--- NavigationControllerPractice/NavigationControllerPractice/ViewController/RegisterViewController.swift
+++ NavigationControllerPractice/NavigationControllerPractice/ViewController/RegisterViewController.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    @IBOutlet weak var buttonLoginViewController: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 상단 네비게이션 바 부분을 숨김 처리
+        self.navigationController?.isNavigationBarHidden = true
     }
+    
+    @IBAction func onLoginButtonClicked(_ sender: UIButton) {
+        print("RegisterViewController - onLoginViewControllerButtonClicked() called / 로그인 버튼 클릭")
+        
+        // 네비게이션 뷰 컨트롤러를 팝 하면 로그인 화면이 나오게 됨
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
